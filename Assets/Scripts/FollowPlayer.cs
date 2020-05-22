29b12685d77f2f602d1463778721bb0cbe06cc65
Tuan Tran <sgs@sgss-Mac-mini-2.local>
2020-05-22 15:54:06 +0700

    Add player and ground

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x > 0)
+        {
+            transform.position = player.transform.position + new Vector3(0, 0, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 10;
+    private Animator playerAnim;
+    private Rigidbody2D playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+        ChangeAnim();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 localScale = transform.localScale;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // transform.Translate(-speed * Time.deltaTime * Vector3.right);
+            playerRb.AddForce(-speed * Time.deltaTime * Vector3.right, ForceMode2D.Impulse);
+            if (localScale.x > 0f)
+            {
+                localScale.x *= -1f;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // transform.Translate(speed * Time.deltaTime * Vector3.right);
+            playerRb.AddForce(speed * Time.deltaTime * Vector3.right, ForceMode2D.Impulse);
+            if (localScale.x < 0f)
+            {
+                localScale.x *= -1f;
+            }
+        }
+    }
+
+    void ChangeAnim()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerAnim.SetBool("isIdling", false);
+            playerAnim.SetBool("isWalking", true);
+            if (playerAnim.GetInteger("Speed_i") > 5)
+            {
+                playerAnim.SetBool("isWalking", false);
+                playerAnim.SetBool("isRunning", true);
+            }
+            else
+            {
+                playerAnim.SetBool("isWalking", true);
+                playerAnim.SetBool("isRunning", false);
+            }
+        }
+        else
+        {
+            playerAnim.SetBool("isIdling", true);
+            playerAnim.SetBool("isWalking", false);
+        }
+    }
+}
