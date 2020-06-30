d4cb17d2de09d7b5cd57ae21a9867d3066a0a6b8
Tieuphi <tieuphi99@gmail.com>
2020-06-30 18:13:28 +0700

    add finish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 410f;
+    public float slideDownSpeed = 410f;
     public float jumpForce = 795f;
     [Range(0, 1)] public float smoothTime = 0.6f;
 
@@ -18,7 +19,9 @@ public class PlayerController : MonoBehaviour
     public bool isDead;
     private bool _isOnGround = true;
     private bool _isEatable;
-    private Vector3 _velocity = Vector3.zero;
+    private bool _isFinish;
+    private bool _isNotHugPole;
+    private Vector3 _velocity;
 
     private Animator _playerAnim;
     private Rigidbody2D _playerRb;
@@ -32,12 +35,15 @@ public class PlayerController : MonoBehaviour
     private static readonly int JumpTrig = Animator.StringToHash("Jump_trig");
     private static readonly int DieB = Animator.StringToHash("Die_b");
     private static readonly int BigB = Animator.StringToHash("Big_b");
+    private static readonly int HugB = Animator.StringToHash("Hug_b");
 
     void Awake()
     {
+        _velocity = Vector3.zero;
         _playerAnim = GetComponent<Animator>();
         _playerRb = GetComponent<Rigidbody2D>();
         isDead = false;
+        _isFinish = false;
     }
 
     private void FixedUpdate()
@@ -46,17 +52,43 @@ public class PlayerController : MonoBehaviour
         {
             Die();
         }
-        else
+        else if (!isDead && !_isFinish)
         {
             MovePlayer();
             GetPlayerSpeed();
+        }
+
+        if (_isFinish)
+        {
+            _playerAnim.SetBool(HugB, true);
+            if (transform.position.y > 0.953f)
+            {
+                transform.Translate(slideDownSpeed * Time.deltaTime * Vector3.down);
+            }
+            else
+            {
+                if (transform.position.x < 192.9768f)
+                {
+                    _playerAnim.SetBool(HugB, false);
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    transform.position = new Vector3(192.9768f, transform.position.y);
+                }
+
+                _playerRb.isKinematic = false;
+                StartCoroutine(HugPole());
+                if (transform.position.x < 198.5f && _isNotHugPole)
+                {
+                    transform.localScale = Vector3.one;
+                    _playerAnim.SetFloat(SpeedF, 9.1f);
+                    transform.Translate(slideDownSpeed * Time.deltaTime * Vector3.right);
+                }
+            }
         }
     }
 
     void MovePlayer()
     {
         Vector3 localScale = transform.localScale;
-
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         Vector2 playerVelocity = _playerRb.velocity;
         Vector3 targetVelocity = new Vector2(horizontalInput * speed * Time.fixedDeltaTime, playerVelocity.y);
@@ -105,6 +137,8 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        Debug.Log(other.gameObject.tag);
+
         if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Pipe") ||
             other.gameObject.CompareTag("Brick") ||
             other.gameObject.CompareTag("Stone"))
@@ -120,6 +154,13 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(SetBoolEatable());
         }
 
+        if (other.gameObject.CompareTag("Pole"))
+        {
+            _isFinish = true;
+            _playerRb.velocity = Vector2.zero;
+            _playerRb.isKinematic = true;
+        }
+
         if (!other.gameObject.CompareTag("BigMushroom") || !_isEatable) return;
         tag = "BigPlayer";
         _playerAnim.SetBool(BigB, true);
@@ -133,5 +174,11 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
         _isEatable = true;
+    }
+
+    IEnumerator HugPole()
+    {
+        yield return new WaitForSeconds(1.2f);
+        _isNotHugPole = true;
     }
 }
